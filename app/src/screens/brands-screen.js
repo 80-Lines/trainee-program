1c2f41008287380e1d74e23a25a6265fa87cd7a7
Henrique Ramos <chenrique.ramos12@gmail.com>
2021-07-13 15:19:45 -0300

    setup react router

---
+++ app/src/screens/brands-screen.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const BrandsScreen = () => {
+  return (
+    <div>
+      <h1>Marcas</h1>
+    </div>
+  );
+};
+
+export default BrandsScreen;
---
+++ app/src/screens/cars-screen.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CarrosScreen = () => {
+  return <h1>Carros</h1>;
+};
+
+export default CarrosScreen;
