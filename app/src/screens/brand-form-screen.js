d94127eab6e5adb500da4bdc9e293ce0d5e48909
Juan Fernandes <juan.jcf@outlook.com>
2021-07-20 16:12:46 -0300

    Added use-form hook; Changes to brand-form-screen

--- app/src/screens/brand-form-screen.js
+++ app/src/screens/brand-form-screen.js
@@ -1,41 +1,41 @@
 import React from "react";
-import Container from "../components/container";
-import Separator from "../components/separator";
-import Input from "../components/input";
-import Button from "../components/button";
-import { useToast } from "../components/toast";
 import { useHistory, useParams } from "react-router-dom";
-import saveBrandService from "../services/save-brand-service";
+import Button from "../components/button";
+import Container from "../components/container";
+import Input from "../components/input";
+import Separator from "../components/separator";
+import { useToast } from "../components/toast";
+import useForm from "../hooks/use-form";
 import getBrandByIdService from "../services/get-brand-by-id-service";
+import saveBrandService from "../services/save-brand-service";
 
 const BrandFormScreen = () => {
-  const [brandId, setBrandId] = React.useState();
-  const [brandName, setBrandName] = React.useState("");
   const { notify } = useToast();
   const { goBack } = useHistory();
-
   const { id } = useParams();
 
-  const saveBrand = () => {
-    const message = id
-      ? `Marca ${brandName} editada com sucesso!`
-      : `Marca ${brandName} adicionada com sucesso!`;
+  const { getValue, setValue, submit } = useForm({
+    initialValues: {},
+    onSubmit: ({ brand }) => {
+      const { id, name } = brand;
+      const message = id
+        ? `Marca ${name} editada com sucesso!`
+        : `Marca ${name} adicionada com sucesso!`;
 
-    saveBrandService({ id, name: brandName }).then(() => {
-      notify({
-        intent: "success",
-        message,
+      saveBrandService({ id, name }).then(() => {
+        notify({
+          intent: "success",
+          message,
+        });
+        goBack();
       });
-      setBrandName("");
-      goBack();
-    });
-  };
+    },
+  });
 
   React.useEffect(() => {
     if (id) {
       getBrandByIdService({ id }).then((data) => {
-        setBrandId(data.id);
-        setBrandName(data.name);
+        setValue("brand", data)
       });
     }
   }, [id]);
@@ -52,10 +52,10 @@ const BrandFormScreen = () => {
         style={{ maxWidth: 500 }}
         onSubmit={(event) => {
           event.preventDefault();
-          saveBrand();
+          submit();
         }}
       >
-        <Input id="id" disabled value={brandId} />
+        <Input id="id" disabled value={getValue("brand.id")} />
         <Separator />
         <label htmlFor="name">
           <b>nome:</b>
@@ -63,8 +63,8 @@ const BrandFormScreen = () => {
         <Separator />
         <Input
           id="name"
-          value={brandName}
-          onChange={(value) => setBrandName(value)}
+          value={getValue("brand.name")} //form.brand.name // form['brand']['name']
+          onChange={(value) => setValue("brand.name", value)}
           required
         />
         <Separator />
--- app/src/screens/brands-screen.js
+++ app/src/screens/brands-screen.js
@@ -1,20 +1,24 @@
 import React from "react";
-import Table from "../components/table";
-import Separator from "../components/separator";
+import { Link } from "react-router-dom";
 import Button from "../components/button";
 import Container from "../components/container";
-import Modal from "../components/modal";
-import { Link } from "react-router-dom";
 import DeleteConfirmationModal from "../components/delete-confirmation-modal";
+import Input from "../components/input";
+import Modal from "../components/modal";
+import Separator from "../components/separator";
+import Table from "../components/table";
 import useBrands from "../hooks/use-brands";
 
 const BrandsScreen = () => {
   const { brands } = useBrands();
+  const [filter, setFilter] = React.useState("");
   const [deletingBrand, setDeletingBrand] = React.useState();
 
   const onRequestClose = () => {
     setDeletingBrand(undefined);
   };
+
+  const filteredBrands = brands.filter(brand => brand.name.startsWith(filter));
 
   return (
     <Container>
@@ -31,8 +35,11 @@ const BrandsScreen = () => {
         </Link>
       </div>
       <Separator />
+      <div>Filtrar por nome</div>
+      <Input value={filter} onChange={(value) => setFilter(value)} />
+      <Separator />
       <Table
-        data={brands}
+        data={filteredBrands}
         columns={[
           { path: "id", label: "#", width: "5%" },
           { path: "name", label: "Nome", width: "90%" },
@@ -70,7 +77,7 @@ const BrandsScreen = () => {
             brand={deletingBrand}
             onCancel={() => onRequestClose()}
             onSuccess={() => {
-              getBrands();
+              // getBrands();
               onRequestClose();
             }}
           />
